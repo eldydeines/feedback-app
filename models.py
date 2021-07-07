f108db2e6f237c156206a7b8c7835825cea5d2f1
Eldy Deines <eldy.deines@outlook.com>
2021-07-07 16:04:56 -0600

    added user model

---
+++ models.py
@@ -0,0 +1,19 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def connect_db(app):
+    """Connect to database."""
+    db.app = app
+    db.init_app(app)
+
+
+class User(db.Model):
+
+    __tablename__ = 'users'
+
+    username = db.Column(db.String(20), primary_key=True, nullable=False,  unique=True)
+    password = db.Column(db.Text, nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    first_name = db.Column(db.String(30), nullable=False)
+    last_name = db.Column(db.String(30), nullable=False)
